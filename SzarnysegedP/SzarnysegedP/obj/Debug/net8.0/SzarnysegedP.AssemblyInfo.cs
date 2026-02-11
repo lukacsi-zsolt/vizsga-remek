8252d0eecd9102cd9680f13e753a5b08d12127f3
lukacsi-zsolt <lukacsi.zsolt@ganziskola.hu>
2026-02-11 14:02:16 +0100

    kisseb modositasok kepek beillesztve stb

--- SzarnysegedP/SzarnysegedP/obj/Debug/net8.0/SzarnysegedP.AssemblyInfo.cs
+++ SzarnysegedP/SzarnysegedP/obj/Debug/net8.0/SzarnysegedP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SzarnysegedP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb3b6904059efbbc2395e345ab7fb4c04edd71ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aff08a3b1f76bd52e2573a1eb697517a5f00caa6")]
 [assembly: System.Reflection.AssemblyProductAttribute("SzarnysegedP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SzarnysegedP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
