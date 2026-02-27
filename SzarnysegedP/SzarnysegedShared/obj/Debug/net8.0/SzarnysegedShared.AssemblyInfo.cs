7a28bf87568873425ce17f36700e0330bc6d2705
szaboflora <szabo.flora@ganziskola.hu>
2026-02-27 14:54:48 +0100

    Aprobb hibak kijavitasa

--- SzarnysegedP/SzarnysegedShared/obj/Debug/net8.0/SzarnysegedShared.AssemblyInfo.cs
+++ SzarnysegedP/SzarnysegedShared/obj/Debug/net8.0/SzarnysegedShared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SzarnysegedShared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+938c9c798f6cedc9ef3a8f0c67af007dda274962")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f484a291ab38d6769a8e4293c486d234bb43c9a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SzarnysegedShared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SzarnysegedShared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
